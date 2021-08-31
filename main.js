ed80dcd5b47f095f673c9540f5d737d70673213c
Caroline Hsu <chsu@hubspot.com>
2021-08-30 21:52:35 -0700

    test scrolling

---
+++ main.js
@@ -0,0 +1,22 @@
+const scrollElements = document.querySelectorAll(".header");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      console.log("Scrolling");
+    } 
+  })
+}
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
