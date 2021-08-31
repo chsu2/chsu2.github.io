73154df507251759050370d75d0a258108b073a4
Caroline Hsu <chsu@hubspot.com>
2021-08-30 23:28:29 -0700

    add method

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // grab elements for scrolling
-const scrollElements = document.querySelectorAll(".heading");
+const scrollElement = document.querySelector(".heading");
 
 const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
@@ -11,15 +11,9 @@ const elementInView = (el, dividend = 1) => {
 };
 
 const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    debugger
-    if (el.scrollTop > 0) {
-      console.log("adding class");
-      el.classList.add("minimized");
-    }
-  })
+  console.log(scrollElement.scrollTop);
 }
 
 window.addEventListener("scroll", () => {
-  console.log("scrolling");
+  handleScrollAnimation();
 });
